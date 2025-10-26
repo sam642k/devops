d58187328969cc663bccddf88a097eb255956a81
Abdus Samee <abdussamee602@gmail.com>
2025-10-27 01:18:52 +0530

    nothing

--- scripts/install-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo dnf update -y
-sudo dnf install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
-echo "Hello DevOps from $(hostname -f)" | sudo tee /usr/share/nginx/html/index.html
